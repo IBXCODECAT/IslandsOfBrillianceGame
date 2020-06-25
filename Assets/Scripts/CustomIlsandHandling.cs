5ad78f3084324acfb205d375f0efb191b5874eb8
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2020-06-25 11:19:49 -0500

    Quickery Skybox

---
+++ Assets/Scripts/CustomIlsandHandling.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomIlsandHandling : MonoBehaviour
+{
+    public GameObject water;
+    public GameObject windowsWater;
+    public GameObject light;
+    public GameObject windowsLight;
+
+    public Material skybox;
+    public Material windowsSkybox;
+
+    void OnEnable()
+    {
+        water.SetActive(false);
+        windowsWater.SetActive(true);
+        light.SetActive(false);
+        windowsLight.SetActive(true);
+        RenderSettings.skybox = windowsSkybox;
+
+    }
+
+    void OnDisable()
+    {
+        water.SetActive(true);
+        windowsWater.SetActive(false);
+        light.SetActive(true);
+        windowsLight.SetActive(false);
+        RenderSettings.skybox = skybox;
+    }
+}
