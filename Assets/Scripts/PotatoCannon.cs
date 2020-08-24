3f4ffca5ccdb1ea32a14eb4dc06d75a256825064
buttejac000 <61168291+buttejac000@users.noreply.github.com>
2020-08-24 17:33:55 -0500

    Potato Cannon Alpha development

---
+++ Assets/Scripts/PotatoCannon.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoCannon : MonoBehaviour
+{
+
+    public GameObject potatoAlpha;
+    public Transform potatoPosition;
+    public float potatoFire;
+
+    public bool cannonFire;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && !cannonFire)
+        {
+            cannonFire = true;
+            StartCoroutine(CannonFire());
+        }
+
+        IEnumerator CannonFire() 
+        {
+            GameObject newpotatoAlpha =
+                Instantiate(potatoAlpha,
+                potatoPosition.position,
+                potatoPosition.rotation);
+            yield return new
+                WaitForSeconds(potatoFire);
+            cannonFire = false;
+        }
+
+    }
+
+
+
+
+
+
+}
