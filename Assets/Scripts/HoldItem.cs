e1c1ac4111f3c90c5d95c3c75e60356f1340ba6a
IBXCODECAT <nathan.schmitt7@gmail.com>
2020-08-24 17:29:23 -0500

    ??

--- Assets/Scripts/HoldItem.cs
+++ Assets/Scripts/HoldItem.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class HoldItem : MonoBehaviour
 {
-    public GameObject[] pickupableItems;
     public KeyCode interact;
+    public Transform player;
+    public Vector3 offset;
+    public GameObject objHold;
+    
+    private float distance;
+    private GameObject target;
 
     void Update()
     {
-        
+        RaycastHit hit;
+        Physics.Raycast(player.position + offset, transform.forward, out hit, Mathf.Infinity);
+
+        if (Vector3.Distance(hit.point, player.position) > distance)
+        {
+            if (hit.transform.gameObject.tag == "PickMeUpBro")
+            {
+                hit.transform.GetComponent<Interactables>();
+                hit.transform.SetParent(objHold.transform, true);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Interactables.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+
+    public virtual void interact()
+    {
+        Debug.LogError("Put a function here you igit");
+    }
+
+}
