b5cd48eb8c7895053dac06996e7dd00b78b7e4b9
IBXCODECAT <nathan.schmitt7@gmail.com>
2020-08-24 17:06:19 -0500

    ?

---
+++ Assets/Scripts/HoldItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldItem : MonoBehaviour
+{
+    public GameObject[] pickupableItems;
+    public KeyCode interact;
+
+    void Update()
+    {
+        
+    }
+}
