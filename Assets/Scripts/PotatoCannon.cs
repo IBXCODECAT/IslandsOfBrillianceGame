b9116d09ba23d0ca7a1747fb05e068039c3a5abf
buttejac000 <61168291+buttejac000@users.noreply.github.com>
2020-08-25 17:15:45 -0500

    Potato Cannon Space Elevator
    The ability to fire potatoes at high rates of fire was successfully integrated, each potato containing a rigid body, and giving the player the ability of flight so to speak. The fire has not been quite perfected, but great progress has been made.

---
+++ Assets/Scripts/PotatoCannon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoCannon : MonoBehaviour
+{
+    public GameObject PotatoAlpha;
+    public Transform potatoPosition;
+    public float potatoFire;
+
+    public float potatoPower;
+
+    public bool cannonFire;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse0) && !cannonFire)
+        {
+            //cannonFire = true;
+              GameObject newPotatoAlpha = Instantiate(PotatoAlpha, potatoPosition.position, potatoPosition.rotation);
+        newPotatoAlpha.GetComponent<Rigidbody>().AddForce(transform.forward * potatoPower);//StartCoroutine(CannonFire());
+        }
+    }
+
+    IEnumerator CannonFire()
+    {
+      
+        yield return new WaitForSeconds(potatoFire);
+        cannonFire = false; 
+    }
+}
