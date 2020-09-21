1a994ecec9e053763799d59fc75e4a7be6fb36ab
IBXCODECAT <nathan.schmitt7@gmail.com>
2020-09-21 16:40:10 -0500

    WINY

---
+++ Assets/SkyboxRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+        //To set the speed, just multiply Time.time with whatever amount you want.
+    }
+}
